428fc1270953adc7381b4d84656d27dcd41b5492
buthed <temaTihonov@yandex.ru>
2021-04-11 13:34:17 +0300

    Ver2

--- src/com/tematihonov/Main.java
+++ src/com/tematihonov/Main.java
@@ -11,13 +11,55 @@ public class Main {
 
         private Node root;
 
+
+        // Task 6.3
+        // Реализуйте методы поиска и вставки узла в дерево.
         public void insert(int value) {
-
+            Node node = new Node();
+            node.setValue(value);
+            if(root == null){
+                root = node;
+            } else {
+                Node current = root;
+                Node parent;
+                while (true) {
+                    parent = current;
+                    if (value < node.getValue()) {
+                        current = current.leftChild;
+                        if (current == null) {
+                            parent.leftChild = node;
+                            return;
+                        }
+                    } else {
+                        current = current.rightChild;
+                        if (current == null) {
+                            parent.rightChild = node;
+                            return;
+                        }
+                    }
+                }
+            }
         }
 
-        public int find (int id) {
 
+        public Node find (int key) {
+            Node current = root;
+            while (current.value != key) {
+                if (key < current.value) {
+                    current = current.leftChild;
+                } else {
+                    current = current.rightChild;
+                }
+                if (current == null) {
+                    return null;
+                }
+            }
+            return current;
         }
+
+
+        // Task 6.5
+        // Реализуйте метод удаления узла и выполните оценку времени базовых методов дерева с помощью System.nanoTime().
 
         public boolean delete(int id) {
 
@@ -29,20 +71,41 @@ public class Main {
         private int value;
         private Node leftChild;
         private Node rightChild;
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getLeftChild() {
+            return leftChild;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public void setLeftChild(Node leftChild) {
+            this.leftChild = leftChild;
+        }
+
+        public void setRightChild(Node rightChild) {
+            this.rightChild = rightChild;
+        }
+
+        public Node getRightChild() {
+            return rightChild;
+        }
     }
 
 
 
 
-    // Task 6.3
-    // Реализуйте методы поиска и вставки узла в дерево.
+
 
     // Task 6.4
     // Реализуйте базовые методы обхода дерева и метода дисплей.
     // Реализуйте поиск максимума и минимума.
 
-    // Task 6.5
-    // Реализуйте метод удаления узла и выполните оценку времени базовых методов дерева с помощью System.nanoTime().
 
     // Task 6.6
     // Реализуйте на основе массива из задания 2.1 алгоритм пирамидальной сортировки с реализацией бинарной пирамиды.
---
+++ src/com/tematihonov/RedBook.java
@@ -0,0 +1,16 @@
+package com.tematihonov;
+
+public class RedBook {
+    String name;
+    int id;
+    int value;
+
+
+    public RedBook(String name, int id, int value) {
+        this.name = name;
+        this.id = id;
+        this.value = value;
+    }
+
+
+}
